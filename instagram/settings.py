a49debc1f8f45ea8b268ada66a7bcb14190e71a9
gabrielcoder <gabrielcoder247@gmail.com>
2018-10-07 12:22:27 +0300

    create navbar.html template

--- instagram/settings.py
+++ instagram/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'instaclone',
+    'bootstrap3',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
