1d8d3150a2376d938632e236a6e69caaf519f23e
gabrielcoder <gabrielcoder247@gmail.com>
2018-10-08 05:02:28 +0300

    create views functions for newprofileform an newimageform

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -5,11 +5,12 @@ from . import views
 
 urlpatterns =[
    
-    url(r'^$',views.home,name='homePage'),
+    url(r'^$',views.home,name='home_page'),
     # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name='pastNews'),
-    url(r'^profile/',views.profile,name ='profilePage'),
+    url(r'^profile/',views.profile,name ='profile_page'),
     url(r'^search/', views.search_results,name='search_results'),
-    # url(r'^new/article$', views.new_article, name='new-article') 
+    url(r'^new/profile$', views.new_profile, name='new_profile') 
+    url(r'^new/image$',views.new_image,name='new_image'),
 ]
 
 if settings.DEBUG:
--- instaclone/views.py
+++ instaclone/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from . models import Image,Profile
 from .email import send_welcome_email
+from .form import NewProfileForm,NewImageForm
 # from friendship.models import Friend, Follow
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 
@@ -39,4 +40,33 @@ def search_results(request):
     else:
 
         message = "You haven't searched for any term"
-        return render(request, 'search.html', {"message":message})      
+        return render(request, 'search.html', {"message":message}) 
+
+
+@login_required(login_url='/accounts/login')
+def new_profile(request):
+	current_user = request.user
+	if request.method == 'POST':
+		form = NewProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('current_profile')
+
+	else:
+			form = NewProfileForm()
+	return render(request, 'new_profile.html',{"form":form })
+
+
+@login_required(login_url='/accounts/login')
+def new_image(request):
+	current_user = request.user
+	if request.method == 'POST':
+		form = NewImageForm(request.POST,request.FILES)
+		if form.is_valid():
+			article = form.save(commit=False)
+			article.user = current_user
+			article.save()
+			return redirect('current_profile')
+	else:
+			form = NewImageForm()
+	return render(request, 'new_image.html',{"form":form })
