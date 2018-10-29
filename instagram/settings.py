4d3c40a6fed3efe214254e03fdb126afeab53347
gabrielcoder <gabrielcoder247@gmail.com>
2018-10-29 21:09:44 +0300

    pushing to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfileConfigs',
+    'staticfiles',
 ]
 
 MIDDLEWARE = [
