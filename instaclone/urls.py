79cf04e994f2054d3fa5843cc3b3476da69c820c
gabrielcoder <gabrielcoder247@gmail.com>
2018-10-29 21:44:55 +0300

    pushing to heroku

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url,include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from . import views as core_views
 
 urlpatterns =[
 
